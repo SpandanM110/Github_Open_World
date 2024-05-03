eae8ecd5312da35455a7c1d4f451f41780c8637b
SpandanM110 <spandanmukherjee2003@gmail.com>
2024-05-04 00:20:22 +0530

    Add BackgroundBeamsDemo component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import {NavbarDemo} from "./src/components/Navbar";
 import {HeroScrollDemo} from "./src/components/Scroll";
 import {GlobeDemo} from "./src/components/globe";
 import {WobbleCardDemo} from "./src/components/card";
+import {BackgroundBeamsDemo} from "./src/components/waitlist";
 
 
 export default function Home() {
@@ -11,7 +12,14 @@ export default function Home() {
      <NavbarDemo />
       <HeroScrollDemo />
       <GlobeDemo />
+      
       <WobbleCardDemo />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <BackgroundBeamsDemo />
       
 
      
