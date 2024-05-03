2c49ad0046cf68b2edf6efb2712e00e3e31b84c0
SpandanM110 <spandanmukherjee2003@gmail.com>
2024-05-04 00:01:53 +0530

    Add GlobeDemo and WobbleCardDemo components to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 
 import {NavbarDemo} from "./src/components/Navbar";
 import {HeroScrollDemo} from "./src/components/Scroll";
+import {GlobeDemo} from "./src/components/globe";
+import {WobbleCardDemo} from "./src/components/card";
+
 
 export default function Home() {
   return (
     <main>
      <NavbarDemo />
       <HeroScrollDemo />
+      <GlobeDemo />
+      <WobbleCardDemo />
+      
+
      
       
       
---
+++ app/src/fonts/calsans.ts
@@ -0,0 +1,13 @@
+// calsans.ts
+
+// Define a function that returns font-related styles or configurations
+const getCalsansFont = () => {
+    return {
+      className: "font-calsans", // CSS class name for calsans font
+      // Other font-related configurations if needed
+    };
+  };
+  
+  // Export the function to be used in other files
+  export { getCalsansFont };
+  
